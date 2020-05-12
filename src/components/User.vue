9287616325021c46f607957ac042037ba51dac79
xiaoshajicoder <1157974665@qq.com>
2020-05-12 11:36:53 +0800

    2020.5.12 router相关知识

---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>我是我的</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeRouteEnter(to, from, next) {
+    console.log("组件内进入守卫");
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log("组件内离开守卫");
+    next();
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Home = () => import('../components/Home.vue')
+const User = () => import('../components/User.vue')
+
+const routes = [
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/user',
+        component: User,
+        beforeEnter: function (to, from, next) {
+            console.log("我是user的路由独享守卫")
+            next()
+        }
+    }
+]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history',
+
+})
+
+router.beforeEach((to, from, next) => {
+    // setTimeout(() => console.log('我是全局前置钩子'), 1000)
+
+    // console.log(from)
+    // console.log(to)
+
+    console.log('我是全局前置钩子')
+
+    next()
+
+})
+
+router.beforeResolve((to, from, next) => {
+    console.log('我是全局解析守卫')
+    next()
+})
+
+router.afterEach((to, from) => {
+    console.log('我是全局后置钩子')
+
+})
+
+export default router
